c7c62a95ac7b2655635af790790c340daa826b31
Anastasiia <vostrova_1994@mail.ru>
2022-05-22 18:19:46 +0300

    Fix Crud OrderUnit

--- CoCoCoworking.DB/Stored Procedures/OrderUnit_Update.sql
+++ CoCoCoworking.DB/Stored Procedures/OrderUnit_Update.sql
@@ -1,2 +1,27 @@
-﻿CREATE APPLICATION ROLE [OrderUnit_Update]
-	WITH PASSWORD = 'UoiGOjasllxsjmcvz{uzroA9msFT7_&#$!~<3ffnoWjueGK|'
+﻿CREATE PROCEDURE [dbo].[OrderUnit_Update]
+	
+	@Id int,
+	@StartDate nvarchar(255),
+	@EndDate nvarchar(255),
+	@RoomId int,
+	@WorkPlaceId int,
+	@WorkPlaceInRoomId int,
+	@AdditionalServiceId int,
+	@OrderId int
+
+AS
+BEGIN
+	
+	UPDATE [dbo].[OrderUnit]
+
+	SET Id= @Id,
+	StartDate= @StartDate,
+	EndDate= @EndDate,
+	RoomId= @RoomId,
+	WorkPlaceId=@WorkPlaceId,
+	WorkPlaceInRoomId=@WorkPlaceInRoomId,
+	AdditionalServiceId=@AdditionalServiceId,
+	OrderId=@OrderId
+
+	WHERE Id = @Id
+END
