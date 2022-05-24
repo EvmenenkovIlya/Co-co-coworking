f1bf81ac51e41ea4a59d90d5f4cd2d003b083bb2
alkanik <alkanik@gmail.com>
2022-05-25 02:00:30 +0300

    Add SP FinancialReport

---
+++ CoCoCoworking.DB/Stored Procedures/GetFinanceReport.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[GetFinancialReport]
+	@Date DATE
+AS
+BEGIN
+	SELECT OU.[RoomId], OU.[WorkPlaceId], OU.[AdditionalServiceId], SUM(OU.[OrderUnitCost]) as Summ FROM [dbo].[Order] as O
+	join [dbo].[OrderUnit] as OU on OU.[OrderId] = O.[Id]
+	WHERE MONTH(CAST(O.[PaidDate] AS DATE)) = MONTH(@Date)
+	GROUP BY OU.[RoomId], OU.[WorkPlaceId], OU.[AdditionalServiceId]
+	
+END
