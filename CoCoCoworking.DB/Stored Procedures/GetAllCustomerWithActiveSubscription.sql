b3de3c9b70c5ccd2236f0fa5be5b6f11637a94b6
alkanik <alkanik@gmail.com>
2022-05-25 00:30:15 +0300

    Add condition AdService = subscription

--- CoCoCoworking.DB/Stored Procedures/GetAllCustomerWithActiveSubscription.sql
+++ CoCoCoworking.DB/Stored Procedures/GetAllCustomerWithActiveSubscription.sql
@@ -5,5 +5,5 @@ BEGIN
 	SELECT C.[Id], C.[FirstName], C.[LastName], C.[PhoneNumber], O.[Id], OU.[Id], OU.[AdditionalServiceId], OU.[StartDate], OU.[EndDate] FROM [dbo].[Customer] as C
 	join [dbo].[Order] as O on O.[CustomerId] = C.[Id]
 	join [dbo].[OrderUnit] as OU on OU.[OrderId] = O.[Id]
-	WHERE CAST(OU.[EndDate] AS DATE) >= GETDATE()
+	WHERE OU.[AdditionalServiceId] = 1 AND CAST(OU.[EndDate] AS DATE) >= GETDATE()
 END
