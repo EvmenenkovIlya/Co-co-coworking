6f92f96d049b53c724febcc7362c984332706c92
alkanik <alkanik@gmail.com>
2022-05-25 02:16:49 +0300

    Add Count for Room, WorrkPlace, AService

--- CoCoCoworking.DB/Stored Procedures/GetFinanceReport.sql
+++ CoCoCoworking.DB/Stored Procedures/GetFinanceReport.sql
@@ -2,7 +2,14 @@
 	@Date DATE
 AS
 BEGIN
-	SELECT OU.[RoomId], OU.[WorkPlaceId], OU.[AdditionalServiceId], SUM(OU.[OrderUnitCost]) as Summ FROM [dbo].[Order] as O
+	SELECT OU.[RoomId], 
+		COUNT(OU.[RoomId]) as RoomCount, 
+		OU.[WorkPlaceId], 
+		COUNT(OU.[WorkPlaceId]) as WorkPlaceCount, 
+		OU.[AdditionalServiceId], 
+		COUNT(OU.[AdditionalServiceId]) as AdditionalServiceCount, 
+		SUM(OU.[OrderUnitCost]) as Summ 
+		FROM [dbo].[Order] as O
 	join [dbo].[OrderUnit] as OU on OU.[OrderId] = O.[Id]
 	WHERE MONTH(CAST(O.[PaidDate] AS DATE)) = MONTH(@Date)
 	GROUP BY OU.[RoomId], OU.[WorkPlaceId], OU.[AdditionalServiceId]
