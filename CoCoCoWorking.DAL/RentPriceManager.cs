c51ec2f2a4242503f61c78b49ff38d4ec09457b0
Lev <leva.100s@gmail.com>
2022-05-25 23:19:30 +0300

    Add Delete and Update in RetntPriceManager

--- CoCoCoWorking.DAL/RentPriceManager.cs
+++ CoCoCoWorking.DAL/RentPriceManager.cs
@@ -15,7 +15,7 @@ namespace CoCoCoWorking.DAL
                 connection.Open();
 
                 return connection.Query<RentPriceDTO>(
-                    StoredProcedures.Customer_GetAll,
+                    StoredProcedures.RentPrice_GetAll,
                     commandType: System.Data.CommandType.StoredProcedure)
                     .ToList();
             }
@@ -27,11 +27,71 @@ namespace CoCoCoWorking.DAL
                 connection.Open();
 
                 return connection.QuerySingle<RentPriceDTO>(
-                    StoredProcedures.Customer_GetById,
+                    StoredProcedures.RentPrice_GetById,
                     param: new { id = id },
-                    commandType: System.Data.CommandType.StoredProcedure
-                    );
+                    commandType: System.Data.CommandType.StoredProcedure);
             }
         }
+
+        public void AddRentPrice(int roomId, int workPlaceInRoomId, int additionalServiceId, string periodType,
+                                               decimal regularPrice, decimal residentPrice, decimal fixedPrice)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                    connection.QuerySingle<RentPriceDTO>(
+                    StoredProcedures.RentPrice_Add,
+                     param: new
+                     {
+                         roomId = roomId,
+                         workPlaceInRoomId = workPlaceInRoomId,
+                         additionalServiceId = additionalServiceId,
+                         periodType = periodType,
+                         regularPrice = regularPrice,
+                         residentPrice = residentPrice,
+                         fixedPrice = fixedPrice
+                     },
+                    commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+        public void UpdateRentPrice(int id, int roomId, int workPlaceInRoomId, int additionalServiceId, string periodType,
+                                                          decimal regularPrice, decimal residentPrice, decimal fixedPrice)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                    connection.QuerySingleOrDefault <RentPriceDTO>(
+                    StoredProcedures.RentPrice_Update,
+                    param: new
+                    {
+                        id = id,
+                        roomId = roomId,
+                        workPlaceInRoomId = workPlaceInRoomId,
+                        additionalServiceId = additionalServiceId,
+                        periodType = periodType,
+                        regularPrice = regularPrice,
+                        residentPrice = residentPrice,
+                        fixedPrice = fixedPrice
+                    },
+                    commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+        public void DeleteRentPrice(int id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                    connection.QuerySingle<RentPriceDTO>(
+                    StoredProcedures.RentPrice_Update,
+                    param: new { id = id },
+                    commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
     }
 }
