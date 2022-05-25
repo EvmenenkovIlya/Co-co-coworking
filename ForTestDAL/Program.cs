a402a043333d70ecd73d286a39c2c6bb2f876820
Ilya <Evmenya@gmail.com>
2022-05-25 23:20:04 +0300

    Finish stored procedures for Customers

--- ForTestDAL/Program.cs
+++ ForTestDAL/Program.cs
@@ -10,5 +10,7 @@ foreach (var i in customers)
     Console.WriteLine(i.ToString());
 }
 
+//CM.AddCustomer("Niko", "Roman", "444-321-555", "NikoDarkStalker@mail.com");
+CM.UpdateCustomer(2,"Roman", "Niko", "555-321-444", "RomanDarkStalker96@mail.com");
 
-Console.WriteLine(CM.GetCustomerByID(1));
+Console.WriteLine(CM.GetCustomerByID(2));
