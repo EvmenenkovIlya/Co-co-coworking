9511fe324a57b0f7cdf812df8ca091dce6d1d5e5
Lev <leva.100s@gmail.com>
2022-05-25 22:19:18 +0300

    Add RentPriceDTO and RentPriceManager

---
+++ CoCoCoWorking.DAL/RentPriceManager.cs
@@ -0,0 +1,37 @@
+﻿using CoCoCoWorking.DAL.DTO;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace CoCoCoWorking.DAL
+{
+    public class RentPriceManager
+    {
+
+        public string connectionString = @"Server=.;Database=CoCoCoworking.DB;Trusted_Connection=True;";
+        public List<RentPriceDTO> GetAllRentPrice()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                return connection.Query<RentPriceDTO>(
+                    StoredProcedures.Customer_GetAll,
+                    commandType: System.Data.CommandType.StoredProcedure)
+                    .ToList();
+            }
+        }
+        public RentPriceDTO GetRentPriceByID(int id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                return connection.QuerySingle<RentPriceDTO>(
+                    StoredProcedures.Customer_GetById,
+                    param: new { id = id },
+                    commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
+    }
+}
