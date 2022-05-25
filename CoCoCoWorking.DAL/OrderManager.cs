f3c52397af81adcfdf0d66ef8b15e2f14dd703f2
Anastasiia <vostrova_1994@mail.ru>
2022-05-25 20:23:27 +0300

    Add DTO Order and started doing manager

---
+++ CoCoCoWorking.DAL/OrderManager.cs
@@ -0,0 +1,34 @@
+﻿using CoCoCoWorking.DAL.DTO;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace CoCoCoWorking.DAL
+{
+    public class OrderManager
+    {
+        public string connectionString = @"Server=localhost;Database=master;Trusted_Connection=True;";
+        public List<OrderDTO> OrderAdd()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+               return connection.Query<OrderDTO>
+                      (StoredProcedures.OrderUnit_Add, commandType:System.Data.CommandType.StoredProcedure)
+                      .ToList();
+            }
+        }
+        public OrderDTO OrderGetById(int id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                return connection.QuerySingle<OrderDTO>(
+                    StoredProcedures.Order_GetById,
+                    param: new { id = id },
+                    commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
+    }
+}
---
+++ проверка/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Threading.Tasks;
+using CoCoCoWorking.DAL;
+using CoCoCoWorking.DAL.DTO;
+
+
+var IU = new OrderManager();
+
+List<OrderDTO> orderAdds = IU.OrderAdd();
+
+foreach (var i in orderAdds)
+{
+    Console.WriteLine(i.ToString());
+}
+
