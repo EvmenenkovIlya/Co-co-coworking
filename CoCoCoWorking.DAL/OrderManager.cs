fc63d738a110dd319717bc1a19b65b3059142017
Anastasiia <vostrova_1994@mail.ru>
2022-05-26 11:07:56 +0300

    Сompleted

--- CoCoCoWorking.DAL/OrderManager.cs
+++ CoCoCoWorking.DAL/OrderManager.cs
@@ -7,14 +7,23 @@ namespace CoCoCoWorking.DAL
     public class OrderManager
     {
         public string connectionString = @"Server=localhost;Database=master;Trusted_Connection=True;";
-        public List<OrderDTO> OrderAdd()
+
+        public OrderDTO OrderAdd(int? CustomerId, decimal OrderCost, string OrderStatus, string PaidDate)
         {
             using (var connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-               return connection.Query<OrderDTO>
-                      (StoredProcedures.OrderUnit_Add, commandType:System.Data.CommandType.StoredProcedure)
-                      .ToList();
+
+                return connection.QuerySingle<OrderDTO>
+                       (StoredProcedures.Order_Add,
+                       param: new
+                       {
+                           CustomerId = CustomerId,
+                           OrderCost = OrderCost,
+                           OrderStatus = OrderStatus,
+                           PaidDate = PaidDate
+                       },
+                       commandType: System.Data.CommandType.StoredProcedure);
             }
         }
         public OrderDTO OrderGetById(int id)
@@ -26,6 +35,37 @@ namespace CoCoCoWorking.DAL
                 return connection.QuerySingle<OrderDTO>(
                     StoredProcedures.Order_GetById,
                     param: new { id = id },
+                    commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
+        public List<OrderDTO> OrderGetAll()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                return connection.Query<OrderDTO>
+                       (StoredProcedures.Order_GetAll,
+                       commandType: System.Data.CommandType.StoredProcedure)
+                       .ToList();
+            }
+        }
+
+        public OrderDTO OrderUpdate(int id, int? CustomerId, decimal OrderCost, int? OrderStatus, int? PaidDate)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                return connection.QuerySingle<OrderDTO>(
+                    StoredProcedures.Order_GetById,
+                    param: new { 
+                        id = id,
+                        CustomerId= CustomerId,
+                        OrderCost= OrderCost,
+                        OrderStatus= OrderStatus,
+                        PaidDate= PaidDate
+                    },
                     commandType: System.Data.CommandType.StoredProcedure
                     );
             }
