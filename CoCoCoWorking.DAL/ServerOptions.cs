923785738a525ddbe56365bc8bc6637480d12991
Ilya Bozhkov <ilia.il.27@mail.ru>
2022-05-30 15:51:31 +0300

    Auto stash before merge of "CC-52" and "origin/CC-52"

--- CoCoCoWorking.DAL/ServerOptions.cs
+++ CoCoCoWorking.DAL/ServerOptions.cs
@@ -4,6 +4,7 @@
     {
         private const string _ilyaEServer = @"Server=.;Database=CoCoCoworking.DB;Trusted_Connection=True;";
         private const string _LevServer = @"Server=DESKTOP-U9ABOQU\SQLEXPRESS;Database=CoCoCoworking.DB;Trusted_Connection=True;";
-        public const string ConnectionOption = _LevServer;
+        private const string _IlyaBServer = @"Server=LAPTOP-SLNPF1AK;Database=CoCoCoworking.DB;Trusted_Connection=True;";
+        public const string ConnectionOption = _IlyaBServer;
     }
 }
