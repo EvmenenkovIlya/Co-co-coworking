ccebb11632c3a8cefeafa223dcc90e4096d758a6
alkanik <alkanik@gmail.com>
2022-05-30 22:40:44 +0300

    change SP GetFinancialReport - add names room&servises + count all WP

--- CoCoCoWorking.DAL/ServerOptions.cs
+++ CoCoCoWorking.DAL/ServerOptions.cs
@@ -5,6 +5,6 @@
         private const string _Ananana = @"Server=DESKTOP-O45J1VO;Database=CoCoCoworking.DB;Trusted_Connection=True;";
         private const string _ilyaEServer = @"Server=.;Database=CoCoCoworking.DB;Trusted_Connection=True;";
         private const string _LevServer = @"Server=DESKTOP-U9ABOQU\SQLEXPRESS;Database=CoCoCoworking.DB;Trusted_Connection=True;";
-        public const string ConnectionOption = _Ananana;
+        public const string ConnectionOption = _ilyaEServer;
     }
 }
