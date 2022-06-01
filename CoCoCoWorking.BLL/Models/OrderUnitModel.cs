f92a30bbb9a488a47fa71936212b17a5be7473db
Emil <intestinal_d@rambler.ru>
2022-06-01 03:15:46 +0300

    Added OrderUnit model

---
+++ CoCoCoWorking.BLL/Models/OrderUnitModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoCoCoWorking.BLL.Models
+{
+    public class OrderUnitModel
+    {
+        public string ProductName { get; set; }
+        public string Name { get; set; }
+        public int WorkPlaceNumber { get; set; }
+        public decimal Price { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+
+    }
+}
