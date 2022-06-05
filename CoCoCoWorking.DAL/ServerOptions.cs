b8906b84598382bb1b2621b2187dde0c708ae653
Emil <intestinal_d@rambler.ru>
2022-06-05 22:28:41 +0300

    Added server option

--- CoCoCoWorking.DAL/ServerOptions.cs
+++ CoCoCoWorking.DAL/ServerOptions.cs
@@ -7,6 +7,9 @@
         private const string _LevServer = @"Server=DESKTOP-U9ABOQU\SQLEXPRESS;Database=CoCoCoworking.DB;Trusted_Connection=True;";
         private const string _IlyaBServer = @"Server=LAPTOP-SLNPF1AK;Database=CoCoCoworking.DB;Trusted_Connection=True;";
         private const string _DaniilServer = @"Server=DESKTOP-3318FA3;Database=CoCoCoworking.DB;Trusted_Connection=True;";
-        public const string ConnectionOption = _LevServer;
+        private const string _EmilServer = @"Server=.;Database=CoCoCoworking.DB;Trusted_Connection=True;";
+
+
+        public const string ConnectionOption = _EmilServer;
     }
 }
