ac74fb050bc76b76e181c9217aa5cfcd819e721d
Ilya Bozhkov <ilia.il.27@mail.ru>
2022-06-07 15:12:14 +0300

    fix rentPriceModel

--- CoCoCoWorking.DAL/DTO/RentPriceDTO.cs
+++ CoCoCoWorking.DAL/DTO/RentPriceDTO.cs
@@ -9,9 +9,9 @@ namespace CoCoCoWorking.DAL.DTO
     public class RentPriceDto
     {
         public int Id { get; set; }
-        public int RoomId { get; set; }
-        public int WorkPlaceInRoomId { get; set; }
-        public int AdditionalServiceId { get; set; }
+        public int? RoomId { get; set; }
+        public int? WorkPlaceInRoomId { get; set; }
+        public int? AdditionalServiceId { get; set; }
         public string PeriodType { get; set; }
         public decimal RegularPrice { get; set; }
         public decimal? ResidentPrice { get; set; }
