f206d6038822edf58e4ea6c8be75130e62657441
Anastasiia <vostrova_1994@mail.ru>
2022-06-09 17:04:22 +0300

    Add in models constructors and equals

--- CoCoCoWorking.BLL/Models/GetAllUnitOrdersFromSpecificOrderModel.cs
+++ CoCoCoWorking.BLL/Models/GetAllUnitOrdersFromSpecificOrderModel.cs
@@ -13,5 +13,32 @@ namespace CoCoCoWorking.BLL.Models
         public string EndDate { get; set; }
         public string Name { get; set; }
         public int? Number { get; set; }
+
+        public GetAllUnitOrdersFromSpecificOrderModel()
+        {
+
+        }
+
+        public override bool Equals(object? obj)
+        {
+            bool flag = true;
+            if (obj == null || !(obj is GetAllUnitOrdersFromSpecificOrderModel))
+            {
+                flag = false;
+            }
+            else
+            {
+                GetAllUnitOrdersFromSpecificOrderModel gaouDto = (GetAllUnitOrdersFromSpecificOrderModel)obj;
+                if (gaouDto.Id != this.Id ||
+                    gaouDto.StartDate != this.StartDate ||
+                    gaouDto.EndDate != this.EndDate ||
+                    gaouDto.Name != this.Name ||
+                    gaouDto.Number != this.Number)
+                {
+                    flag = false;
+                }
+            }
+            return flag;
+        }
     }
 }
--- CoCoCoWorking.BLL/Models/OrderModel.cs
+++ CoCoCoWorking.BLL/Models/OrderModel.cs
@@ -6,5 +6,26 @@
         public int? OrderCost { get; set; }
         public string OrderStatus { get; set; }
         public string PaidDate { get; set; }
-    }
+
+        public override bool Equals(object? obj)
+        {
+            bool flag = true;
+            if (obj == null || !(obj is OrderModel))
+            {
+                flag = false;
+            }
+            else
+            {
+                OrderModel orDto = (OrderModel)obj;
+                if (orDto.OrderCost != this.OrderCost ||
+                    orDto.OrderStatus != this.OrderStatus ||
+                    orDto.PaidDate != this.PaidDate)
+
+                {
+                    flag = false;
+                }
+            }
+            return flag;
+        }
+        }
 }
